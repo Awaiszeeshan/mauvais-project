d9a5bb37c158af2a26b228a9ad0c8778ac09d450
awais <awais@repairdesk.co>
2022-02-10 16:48:33 +0500

    first commit

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import naive from 'naive-ui'
+import "@/assets/styles/custom.scss"
+createApp(App).use(store).use(naive).use(router).mount('#app')
